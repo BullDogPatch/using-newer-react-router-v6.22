32c4225bebbb2d6ec0bec9719ba4a7490d81350c
Craig Clayton <craig__clayton@outlook.com>
2024-02-26 21:19:40 +0000

    modify routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ErrorPage from "./routes/ErrorPage";
-import ProfilesPage from "./routes/ProfilesPage";
 import "./index.css";
 import Home from "./routes/Home";
 import ProfilePage from "./routes/ProfilePage";
+import Root from "./components/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/profiles",
-    element: <ProfilesPage />,
     children: [
+      {
+        element: <Home />,
+        index: true,
+      },
       {
         path: "/profiles/:username",
         element: <ProfilePage />,
