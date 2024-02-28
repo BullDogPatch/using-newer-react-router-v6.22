6ef87e1baa2f7cad45a421abfd9adc51286e68d8
Craig Clayton <craig__clayton@outlook.com>
2024-02-28 19:33:41 +0000

    make fit to screen and sort styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,11 @@
 const Home = () => {
   return (
-    <div className="min-w-[80%] flex justify-center bg-gray-dark">home</div>
+    <div className="min-w-[80%] flex justify-center items-center bg-gray-dark">
+      <div>
+        <h1 className="text-bold text-5xl py-1">Welcome</h1>
+        <p className="text-bold text-3xl">This is just a landing page</p>
+      </div>
+    </div>
   );
 };
 
