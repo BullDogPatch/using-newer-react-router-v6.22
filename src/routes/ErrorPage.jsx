b8d702c5e6079f0896000088613ac5c8438e6016
Craig Clayton <craig__clayton@outlook.com>
2024-02-29 18:22:14 +0000

    changed color of background on error page

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -5,13 +5,16 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <Link to="/">Home</Link>
+    <div className="h-screen flex justify-center items-center bg-gray-dark">
+      <div>
+        {" "}
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Link to="/">Home</Link>
+      </div>
     </div>
   );
 }
